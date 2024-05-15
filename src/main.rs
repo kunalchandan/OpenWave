83d56bbf2decc688bb1ed5eb2d51dca2fe6437b5
Kunal Chandan <kunal_chandan@outlook.com>
2024-05-14 20:28:37 -0700

    CPAL addition

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,30 @@
-slint::include_modules!();
+use std::f32::consts::PI;
 
-slint::slint! {
-    export component LPFWindow {
-        width : 128px;
-        height : 128px;
-        Rectangle {
-            x: 0px;
-            y: 0px;
-            Path {
-                width : parent.width;
-                height: parent.height;
-                stroke: blueviolet;
-                stroke-width: 4px;
-                MoveTo {
-                    x:0;
-                    y:parent.height/2/1px;
-                }
-                LineTo {
-                    x:40;
-                    y:32;
-                }
-                LineTo {
-                    x:44;
-                    y:25;
-                }
-                Close {
-                }
-            }
-        }
-    }
+slint::include_modules!();
+fn _calculate_wavepropagation() {
+    // // Tuning audio for sound at a certain frequency for a listener a certain distance from the speakers
+    // // Speaker Sources:
+    // // Frequency: $ f = 440 Hz$
+    // let compute_buffer: Vec<Vec<f32>> = vec![vec![0.0; 256]; 256];
+    // let unit_distance = 1.0f32; // cm/unit
+    // let sound_sources = vec![vec![128-8, 32], vec![128+8, 32]]; // two devices centered at (128, 32)
 }
 
 fn main() {
     let _ = dotenvy::dotenv();
+
+    let host;
+    #[cfg(target_os = "windows")]
+    {
+        host = cpal::host_from_id(cpal::HostId::Asio).expect("failed to initialise ASIO host");
+    }
+    let f_sampling: u32 = 44_100; // CD frequency
+    let s_lut: Vec<f32> = (0..f_sampling)
+        .map(|x| ((x as f32) * 2.0 * PI / (f_sampling as f32)).sin())
+        .collect();
+
+
     // LPFWindow::new().unwrap().run().unwrap();
-    Keyboard::new().unwrap().run().unwrap();
+    // Keyboard::new().unwrap().run().unwrap();
+    // Dials::new().unwrap().run().unwrap();
 }
